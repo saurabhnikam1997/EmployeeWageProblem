ebea758f288caaf9aae1042eb915fe6b10adb654
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-24 06:44:25 +0530

    Added:different java files to implement empwageprogram using java objects and classes

---
+++ src/com/employee/wage/CompanyEmpWage.java
@@ -0,0 +1,31 @@
+package com.employee.wage;
+
+import java.util.ArrayList;
+
+//pojo class
+class CompanyEmpWage {
+		public final String	company;
+		public final int empRatePerHr;
+		public final int numberOfWorkingDays;
+		public final int maxHrPerMonth;
+		public int totalEmpWage;
+		ArrayList<Integer> dailyWage = new ArrayList<Integer>(); 
+	
+		public CompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth) {
+		    this.company = company;
+		    this.empRatePerHr = empRatePerHr;
+		    this.numberOfWorkingDays = numberOfWorkingDays;
+		    this.maxHrPerMonth = maxHrPerMonth;
+		}
+
+
+		//setter
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
+		}
+		public String toString() {
+		    return "Total employee wages for: " + company + " is " + totalEmpWage;
+		  }
+
+
+}
---
+++ src/com/employee/wage/InEmployeeWageOops.java
@@ -0,0 +1,6 @@
+package com.employee.wage;
+
+interface InEmployeeWageOops {
+	public void addCompanyEmpWage(String Company, int empRatePerHr, int numberOfWorkingDyas, int maxHrPerMonth); 
+
+}
